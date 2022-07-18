4f5af1158b3cede97159abcb192f8f97858c7ee8
may0022 <mariana.ayd@gmail.com>
2022-07-18 12:00:34 -0400

    First Commit From Eclipse

---
+++ eclipse-workspace/javadev/tech_strategio_helloworldsim3/src/tech_strategio_helloworldsim3/HelloWorldSimClass.java
@@ -0,0 +1,9 @@
+package tech_strategio_helloworldsim3;
+
+public class HelloWorldSimClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Sim 3");
+	}
+}
